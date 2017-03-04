f392831e8aad603ad1c4fb2faf8e11a3ef4fc52c
Oleg Kondratenko <olegkondratenko@Olegs-MacBook-Pro.local>
2017-03-04 17:44:32 +0200

    initial scheduler operations

--- scheduler.R
+++ scheduler.R
@@ -1,93 +1,50 @@
-
-
-scheduler.showDay <- function(schedule, req_date) {
-  writeLines(paste("This is schedule for", format(req_date, "%u"), "day of the week"))
+scheduler.showDay <- function(schedule, date) {
+  writeLines(paste("This is schedule for", format(date, "%u"), "day of the week"))
   
   #print the whole day
   lapply( schedule, function(x) { writeLines(paste(names(x), "=", x, collapse=", "))  })
   
-  #return events of a given day
-  #FIXIT: now returns all events
-  return(schedule)
+  # as.Date as date_and_time can be passed
+  res <- scheduler.findEventsByParam(schedule, "date", as.Date(date))
+  
+  return(res)
 }
 
 scheduler.getEventsByDate <- function(schedule, date) {
-  ve <- sapply(schedule, function(x) x[["date"]]) == "04/03/2017"
+  ve <- sapply(schedule, function(x) as.Date(x[["date_time"]])) == date
+  
   return(schedule[ve])
 }
 
-scheduler.getTodaysPastEvents <- function(schedule, date, time){
-   # TODO: switch to Date and Time in Schedule
-  ve <- difftime(sapply(scheduler.getEventsByDate(schedule), function(x) x[["time"]]) <= time
+scheduler.findEventsByParam <- function(schedule, param_name, param_value) {
+  ve <- scheduler.findEventPositionsByParam(schedule, param_name, param_value)
   return(schedule[ve])
 }
 
+scheduler.findEventPositionsByParam <- function(schedule, param_name, param_value) {
+  ve <- NULL
+  if (param_name == "date"){
+    ve <- sapply(schedule, function(x) as.Date(x[["date_time"]])) == param_value
+  }else{
+    ve <- sapply(schedule, function(x) x[[param_name]]) == param_value
+  }
 
-#
-# Sample Data
-#
-scheduler.getSampleSchedule <- function(){
-  schedule <- list()
-  schedule[[1]] <- list(id = 1, date = format(Sys.Date(),"%d/%m/%Y"), time = format(Sys.time(),"%H:%M"), proteins_food = "meat", proteins_mass = 200)
-  #adding 3 seconds to time of first event
-  schedule[[2]] <- list(
-    id = 2, 
-    date = format(Sys.Date(),"%d/%m/%Y"), 
-    time = format(Sys.time()+3,"%H:%M:%S"), 
-    proteins_food = "meat", proteins_mass = 200
-  )
-  
-  return(schedule)  
+  return(ve)
 }
 
-#
-# Tests
-#
+scheduler.execEvent <- function(schedule, schedule_event_id, user_event) {
+  v <- scheduler.findEventPositionsByParam(schedule, "id", schedule_event_id)
+  
+  #expect to find only one element
+  schedule[v][[1]]$user_event <- user_event
 
-scheduler.showDay.test <- function(){
-  schedule <- scheduler.getSampleSchedule()
-  
-  res <- scheduler.showDay(schedule, Sys.Date())
-  if (length(res) != 2) writeLines("scheduler.showDay Failed") else writeLines("scheduler.showDay Pass")
-  if (res[[1]]$id != "1") 
-    writeLines("scheduler.showDay Failed") else writeLines("scheduler.showDay Pass")
-  
+  return(schedule)
 }
 
-schedule.getEventsByDate.test <- function(){
-  schedule <- scheduler.getSampleSchedule()
+scheduler.getTodaysPastEvents <- function(schedule, date_time){
+  event_times <-lapply(scheduler.getEventsByDate(schedule, as.Date(date_time)), function(x) x[["date_time"]])
   
-  schedule[[1]]$date <- "04/03/2017"
-  schedule[[2]]$date <- "05/03/2017"
-  
-  
-  res <-  scheduler.getEventsByDate(schedule, "04/03/2017")
-  if (length(res) != 1) writeLines("scheduler.getEventsByDate Failed") else writeLines("scheduler.getEventsByDate Pass")
-  
-  res <-  scheduler.getEventsByDate(schedule, "05/03/2017")
-  if (length(res) != 1) writeLines("scheduler.getEventsByDate Failed") else writeLines("scheduler.getEventsByDate Pass")
-  
+  return(schedule[difftime(do.call(c, event_times), date_time) <= 0])
 }
 
-scheduler.getTodaysPastEvents.test <- function(){
-  schedule <- scheduler.getSampleSchedule()
-
-  schedule[[1]]$time <- "15:00"
-  schedule[[1]]$date <- "04/03/2017"
-
-  schedule[[2]]$time <- "16:00"
-  schedule[[2]]$date <- "04/03/2017"
-  
-    
-  res <-  scheduler.getTodaysPastEvents(schedule, "04/03/2017", "15:01")
-  if (length(res) != 1) writeLines("scheduler.getTodaysPastEvents Failed") else writeLines("scheduler.getTodaysPastEvents Pass")
-  
-  res <-  scheduler.getTodaysPastEvents(schedule, "04/03/2017", "14:58")
-  if (length(res) != 0) writeLines("scheduler.getTodaysPastEvents Failed") else writeLines("scheduler.getTodaysPastEvents Pass")
-}
-
-
-scheduler.showDay.test()
-schedule.getEventsByDate.test()
-scheduler.getTodaysPastEvents.test()
 
